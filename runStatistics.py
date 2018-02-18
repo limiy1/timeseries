d09711d54f39b6fd514e8989204f98c499800a4f
limiy1 <limiy1@users.noreply.github.com>
2018-02-18 20:57:21 +0100

    add statistics

---
+++ runStatistics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+import datasample
+import simulator
+
+inputFile = sys.argv[1]
+print ('Read from file ' + inputFile + ':')
+myData = datasample.DataSample()
+myData.readFromFile(inputFile)
+listRatio = [0.005, 0.010, 0.015, 0.020, 0.025, 0.030]
+simu = simulator.Simulator(listRatio)
+simu.simulate(myData)
+simu.plotStat()
--- simulator.py
+++ simulator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from sys import stdout
 import datasample
 import matplotlib.pyplot as plt
 
@@ -12,7 +13,7 @@ class Simulator:
    # @dataSample: all the data serie
    # @iActionPos: the pressumed action position
    def simulateOnce(self, dataSample, iActionPos):
-      print("Start simulation with action postion = " + repr(iActionPos))
+      #print("Start simulation with action postion = " + repr(iActionPos))
       for i in range(0, len(self.lRatioList)):
          resTmp = dataSample.searchBack(iActionPos, self.lRatioList[i])
          if (resTmp != None):
@@ -26,19 +27,28 @@ class Simulator:
                j = j-1
 
    def simulate(self, dataSample):
-      for iPos in range(1, dataSample.len()):
+      dataNum = dataSample.len()
+      for iPos in range(1, dataNum):
+         stdout.write("\r[%2.1f%%] Simulating with action postion %d/%d" % (iPos/float(dataNum)*100, iPos , dataNum) )
+         stdout.flush()
          self.simulateOnce(dataSample, iPos)
+      stdout.write("\n")
 
    def plotStat(self):
-      histo = [0] * len(self.lRatioList)
+      histoPos = [0] * len(self.lRatioList)
+      histoNeg = [0] * len(self.lRatioList)
       for key, vList in self.resultMap.iteritems():
          for i in range(0, len(vList)):
             if (vList[i] != None):
-               histo[i] += 1
+               if (vList[i] > 0):
+                  histoPos[i] += 1
+               elif (vList[i] < 0):
+                  histoNeg[i] -= 1
 
       objects = tuple(self.lRatioList)
-      x_pos = range(0, len(histo))
-      plt.bar(x_pos, histo, align='center', alpha=0.5)
+      x_pos = range(0, len(histoPos))
+      plt.bar(x_pos, histoPos, color = 'b', align='center', alpha=0.5)
+      plt.bar(x_pos, histoNeg, color = 'r', align='center', alpha=0.5)
       plt.ylabel('Frequency')
       plt.xticks(x_pos, objects)
       plt.title('Statistics')
